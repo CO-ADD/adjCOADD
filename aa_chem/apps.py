1b113ad4335c4314238424cbacb4871decc11985
jo <joannaweinazh@gmail.com>
2022-11-16 14:57:33 +1000

    change name

--- aa_chem/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AaChemConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'aa_chem'
--- test_sch/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class TestSchConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'test_sch'
