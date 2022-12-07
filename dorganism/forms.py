a833fb46f468ff9bbf1260a7cf1d567a916035ef
jo <joannaweinazh@gmail.com>
2022-12-08 08:53:03 +1000

    correct slug error

--- dorganism/forms.py
+++ dorganism/forms.py
@@ -26,9 +26,9 @@ class CreateOrganism_form(ModelForm):
         self.organism_name=organism_name
         user=user
         super(CreateOrganism_form, self).__init__(*args, **kwargs)
-        self.fields['strain_type'].widget = forms.SelectMultiple(choices= get_DictonaryChoices_byDictClass(Dictionary, Organism.Choice_Dictionary['strain_type']))
+        self.fields['strain_type'].widget = forms.SelectMultiple(choices= get_DictonaryChoices_byDictClass(Dictionary, Organism.Choice_Dictionary['strain_type'], ' | '))
         self.fields['strain_type'].widget.attrs.update({'class': 'form-select', 'size':'5', 'multiple': 'true'})
-        self.fields['strain_panel'].widget = forms.SelectMultiple(choices= get_DictonaryChoices_byDictClass(Dictionary, Organism.Choice_Dictionary['strain_panel']))
+        self.fields['strain_panel'].widget = forms.SelectMultiple(choices= get_DictonaryChoices_byDictClass(Dictionary, Organism.Choice_Dictionary['strain_panel'], ' | '))
         self.fields['strain_panel'].widget.attrs.update({'class': 'form-select', 'size':'5', 'multiple': 'true'})
         self.initial['biologist']= ApplicationUser.objects.filter(username=user)[0]
               
