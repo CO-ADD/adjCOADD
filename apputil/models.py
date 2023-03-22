ac38c9477f7785b4276221605f6e8c526c55767f
Jo-Zh <joannaweinazh@gmail.com>
2023-03-22 20:24:53 +1000

    update ui form

--- apputil/models.py
+++ apputil/models.py
@@ -329,7 +329,7 @@ class Dictionary(AuditModel):
         ]
     #------------------------------------------------
     def __str__(self) -> str:
-        return f"{self.dict_value} {self.dict_desc}"
+        return f"{self.dict_value} <small class='not-visible'> {self.dict_desc} </small>"
 
     #------------------------------------------------
     @classmethod
