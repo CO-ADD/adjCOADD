639635eaee637a6eccba75affcb08d0cff31ab20
Joanna <93627519+Jo-Zh@users.noreply.github.com>
2023-04-04 21:19:27 +1000

    Jzg update (#177)
    * jzg 00
    
    * fix open child table issue, delete culre, update taxonomy
    
    * Updated models (dorganism, ddrug)
    
    * update ddrug.model
    
    * update ddrug.model
    
    * Update
    
    * Model
    
    * update
    
    * dl log, fix index_source nonexit in mic_coadd
    
    * update merge jzg
    
    * Upload MIC-COADD
    
    * update output
    
    * update OrgBatch.save
    
    * UpdateOrgDB
    
    * fix dataexport, change login ui, improve orgdetail ui, nav, filterbar
    
    * Model update
    
    * update detailorganism updateform
    
    * update detailorganism updateform
    
    * update merge
    
    * update
    
    * createstock
    
    * fix stock_create
    
    * clear imgs
    
    ---------
    
    Co-authored-by: JZuegg <j.zuegg@uq.edu.au>
    Co-authored-by: Joanna Zhong <uqwzhon4@uq.edu.au>

--- dorganism/views.py
+++ dorganism/views.py
@@ -354,11 +354,11 @@ def createStock(req, orgbatch_id):
     kwargs={}
     kwargs['user']=req.user
     print(f'createstock {orgbatch_id}')
-    form=Stock_createform(orgbatch_id)
+    form=Stock_createform(initial={"orgbatch_id":orgbatch_id},)
     if req.method=='POST':
         form=Stock_createform(req.POST)
         if form.is_valid():
-            print(req.POST.get("orgbatch_id"))
+            print(f'orgbatch_id {req.POST.get("orgbatch_id")}')
             orgbatch_id=req.POST.get("orgbatch_id")
             stock_type=req.POST.get("stock_type")
             stock_date=req.POST.get("stock_date")
@@ -377,11 +377,11 @@ def createStock(req, orgbatch_id):
                     messages.error(req, f'IntegrityError {err} happens, record may be existed!')
                     return redirect(req.META['HTTP_REFERER'])                
         else:
-            print(f'wrong {form.errors.as_data()}')
-            return redirect(req.META['HTTP_REFERER'])      
+            print(f'wrong {form.errors}')
+            # return redirect(req.META['HTTP_REFERER'])      
         
 
-    return render(req, 'dorganism/organism/batch_stock/stock_c.html', { 'form':form, }) 
+    return render(req, 'dorganism/organism/batch_stock/stock_c.html', { 'form':form, 'orgbatch_id':orgbatch_id }) 
 
 # ---------------------------------------------------------------------------------------------
 @login_required
