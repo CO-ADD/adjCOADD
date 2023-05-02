40d73c051a4d1c2c054433b4068de99d45c4d6fc
Joanna <93627519+Jo-Zh@users.noreply.github.com>
2023-05-02 16:00:18 +1000

    Update v1 (#220)
    * v08.00
    
    * v08.02
    
    * link table
    
    * v08.02
    
    * url_link dict in general table, tested organism, taxonomy headerfields
    
    * pivottabel in orgdetail
    
    * v08.02
    
    * sorting code
    
    * Antibio Table
    
    * AntiBio 2
    
    * update
    
    * show micoadd in organism detail view
    
    * styling miccoadd
    
    * style table
    
    * updatedatatable
    
    * updatedatatable
    
    * update style
    
    * update style
    
    * update style
    
    * code imporve
    
    * fix merge
    
    ---------
    
    Co-authored-by: JZuegg <j.zuegg@uq.edu.au>
    Co-authored-by: Joanna Zhong <uqwzhon4@uq.edu.au>

--- dorganism/templatetags/custom_filters.py
+++ dorganism/templatetags/custom_filters.py
@@ -16,10 +16,7 @@ def is_dict(value):
 def is_list(value):
     return isinstance(value, list)
 
-<<<<<<< HEAD
 @register.filter
 def to_valid_selector(value):
     return "a" + re.sub(r"[\s\.\#\[\]\(\)\+\>\~\=\'\*\^\$]", "_", str(value))
-=======
->>>>>>> main
 
