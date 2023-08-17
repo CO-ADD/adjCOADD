fa56690f992e459c4d97d88f7b64545e1e419e6f
Johannes Zuegg <j.zuegg@uq.edu.au>
2023-08-17 11:50:33 +1000

    Vitek

--- adjcoadd/settings.py
+++ adjcoadd/settings.py
@@ -21,8 +21,8 @@ from datetime import timedelta
 
 VERSION = '1.01 (Development) - JZG'
 # Development : Local/Work/<none>
-#DEVELOPMENT=None
-DEVELOPMENT='Work'
+DEVELOPMENT=None
+#DEVELOPMENT='Work'
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
