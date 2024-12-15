2378cbc5ddf0cd8ee88c49b6bdb8e5422823e72e
JZuegg <j.zuegg@uq.edu.au>
2024-12-15 17:42:53 +1000

    ID

--- dorganism/views.py
+++ dorganism/views.py
@@ -176,13 +176,10 @@ def Organism_DetailView(request, pk):
     id_data_df = get_org_identification_summary(object_.organism_id)
     context["org_id_obj_count"] = len(id_data_df)
     context["org_id_obj"] = id_data_df.values.tolist()
-    #context["org_id_df"] = id_data_df.to_dict('records')
     context["org_id_fields"] = list(id_data_df.columns)
     # if 'Index' in context["org_id_fields"]:
     #     context["org_id_fields"].remove('Index')    # customize HEADER_FIELDS
 
-    print(context["org_id_obj"])
-    #print(context["org_id_fields"])
 
     # context["vitekast_obj"]=SimpleLazyObject(lambda: VITEK_AST.objects.filter(organism=object_.organism_name, astatus__gte=0))
     # context["vitekast_obj_count"]=context["vitekast_obj"].count() if context["vitekast_obj"].count()!=0 else None
