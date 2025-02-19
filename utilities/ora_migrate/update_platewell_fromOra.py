fdc4a33f54f33e0fecb6514366bb46d8de4ed3e9
JZuegg <j.zuegg@uq.edu.au>
2025-02-19 14:33:40 +1000

    MasterWell

--- utilities/ora_migrate/update_platewell_fromOra.py
+++ utilities/ora_migrate/update_platewell_fromOra.py
@@ -276,8 +276,10 @@ def main(prgArgs,djDir):
             mwSQL += f" Fetch First {int(prgArgs.test)} Rows Only "
             nWells = int(prgArgs.test)
         else:
-            mwSQL = "Select * From MasterWell Where Dilution is not Null Order By plate_id, well_id"
-            nWells = CastDB.nCount("Select count(1) From Masterwell Where Dilution is not Null" )
+            # mwSQL = "Select * From MasterWell Where Dilution is not Null Order By plate_id, well_id"
+            # nWells = CastDB.nCount("Select count(1) From Masterwell Where Dilution is not Null" )
+            mwSQL = "Select * From MasterWell Order By plate_id, well_id"
+            nWells = CastDB.nCount("Select count(1) From Masterwell " )
         logger.info(f"{OutName} {nWells} ")
 
         # ---------------------------------------------------------------------------------
