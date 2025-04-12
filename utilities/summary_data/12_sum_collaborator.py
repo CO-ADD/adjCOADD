c209ab229acf8ff45dadefef5e65156707524da3
JZuegg <j.zuegg@uq.edu.au>
2025-04-12 11:53:18 +1000

    sum

--- utilities/summary_data/12_sum_collaborator.py
+++ utilities/summary_data/12_sum_collaborator.py
@@ -60,7 +60,7 @@ def main(prgArgs,djDir):
         #cAnalysis.qry_by_ProjectID(prgArgs.projectid)
         if cAnalysis.n_compounds>0:
             cAnalysis.get_dataframe(SC_Only=prgArgs.sc_only, DR_Only=prgArgs.dr_only)
-            cAnalysis.get_sample_info(Storage_Info=False, Structure_Info=True)
+            cAnalysis.get_sample_info(Storage_Info=True, Structure_Info=True)
             cAnalysis.get_assay_info()
             cAnalysis.get_testplate_info()
             cAnalysis.gen_pivot_tables(PivTables = ['Values','Act'], PivColumns=['assay_org','assay_type','result_type'], PivRows=None)
