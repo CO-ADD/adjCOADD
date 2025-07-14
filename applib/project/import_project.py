46be3c9f60bf262ccb4465443697dd2978ceb71c
Johannes Zuegg <j.zuegg@uq.edu.au>
2025-07-14 17:16:46 +1000

    Project

---
+++ applib/project/import_project.py
@@ -0,0 +1,37 @@
+from django.apps import apps
+
+
+
+def upload_CompoundSubmission_Process(Request, DirName, ExcelFile, ProjectID=None,upload=False,appuser=None):
+    #upload_CompoundSubmission
+    pass
+
+def upload_CompoundSubmission(ProjectID=None,upload=False,appuser=None,valLog=None):
+    #process_CompoundSubmission
+    pass
+
+def process_CompoundSubmission(ProjectID=None,upload=False,appuser=None,valLog=None):
+    #parse_CompoundSubmission
+    pass
+
+def parse_CompoundSubmission(ExcelFile, upload=False,appuser=None,valLog=None):
+    pass
+
+
+def Project_fromDict(iDict,valLog,upload=False):
+    pass
+
+def Compound_fromDict(iDict,valLog,upload=False):
+    pass
+
+def CollabUser_fromDict(iDict,valLog,upload=False):
+    pass
+
+def CollabGroup_fromDict(iDict,valLog,upload=False):
+    pass
+
+def CollabUser_fromDict(iDict,valLog,upload=False):
+    pass
+
+def Organisation_fromDict(iDict,valLog,upload=False):
+    pass
