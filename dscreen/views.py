c8ddc1822604bf22fe8da37d8541fe94d6e59831
JZuegg <j.zuegg@uq.edu.au>
2025-08-22 13:21:47 +1000

    x

--- dscreen/views.py
+++ dscreen/views.py
@@ -182,7 +182,6 @@ class ScreenRun_RemoveView(Base_RemoveView):
     model = Screen_Run
     transaction_use = 'dscreen'
 
-
 # -----------------------------------------------------------------
 @login_required
 def ScreenRun_ReportView(req, pk):
