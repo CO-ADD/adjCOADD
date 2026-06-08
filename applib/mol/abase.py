f4047e03d9558281525d155a5769377b352e8721
Johannes Zuegg <j.zuegg@uq.edu.au>
2026-06-08 15:05:57 +1000

    ABase

--- applib/mol/abase.py
+++ applib/mol/abase.py
@@ -253,11 +253,11 @@ def upload_ABase_Batches(regDF,upload=False,overwrite=False):
             if djUnit:   
                 djABaseCmpBatch.init_amount_unit = djUnit
             else:
+                djABaseCmpBatch.init_amount_unit = None
                 logger.error(f" [Unit] {row['init_value_unit']} not found ")
 
             if row['lab_notebook_number'] is not None:
                 _lab = str(row['lab_notebook_number']).split(chr(160))
-                print(f" [labbook] {_lab}")      
                 djABaseCmpBatch.labbook_no = _lab[0]
                 if len(_lab) > 1:  
                     djABaseCmpBatch.labbook_page = _lab[1]
